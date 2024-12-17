b7cb83eebdc02408659eb13beec2deb2a64eefee
Kemal Bayık <45203337+kemalbayikk@users.noreply.github.com>
2024-12-17 16:45:13 +0000

    library import issue solved

--- src/omero_screen_napari/_train_cnn_widget.py
+++ src/omero_screen_napari/_train_cnn_widget.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 from sklearn.model_selection import train_test_split
 from pathlib import Path
 from tqdm import tqdm
